f0692c283e11a4545c8ad1b40eeb754e71650d4e
EzequielDeiana <ezequielgonzalezdei@gmail.com>
2022-07-25 18:48:49 -0300

    main corrected

--- main.js
+++ main.js
@@ -41,12 +41,24 @@ function closeProductDetailAside() {
 }
 
 const productList=[];
-    productList.push({name:'Bike',price:120,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
-    productList.push({name:'Pantalla',price:220,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
-    productList.push({name:'Compu',price:620,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
-    productList.push({name:'Bike',price:120,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
-    productList.push({name:'Pantalla',price:220,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
-    productList.push({name:'Compu',price:620,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
+    productList.push(
+        {
+            name:'Bike',price:120,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
+    productList.push(
+        {
+            name:'Pantalla',price:220,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
+    productList.push(
+        {
+            name:'Compu',price:620,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
+    productList.push(
+        {
+            name:'Bike',price:120,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
+    productList.push(
+        {
+            name:'Pantalla',price:220,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
+    productList.push(
+        {
+            name:'Compu',price:620,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
 
 function renderProducts(arr){
     for(product of arr){
