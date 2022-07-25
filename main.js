3076966613c12bcaf254c706a82c3668d811c7fc
EzequielDeiana <ezequielgonzalezdei@gmail.com>
2022-07-25 16:05:13 -0300

    mobile menu dekstop menu and cart added

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const burgerButton = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.nav-shopping-cart')
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerButton.addEventListener('click', toggleBurguerMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoMenu);
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+function toggleBurguerMenu(){
+    mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+function toggleCarritoMenu(){
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
 }
