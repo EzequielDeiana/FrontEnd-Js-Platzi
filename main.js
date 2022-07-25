82230c6c522b1f5b00e35c3ab5529744c1dbec5e
EzequielDeiana <ezequielgonzalezdei@gmail.com>
2022-07-25 18:46:01 -0300

    aside corrected

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const burgerButton = document.querySelector('.menu');
-const menuCarritoIcon = document.querySelector('.nav-shopping-cart')
+const menuCarritoIcon = document.querySelector('.nav-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.shopping-cart-detail')
-const cardsContainer = document.querySelector('.cards-container')
+const aside = document.querySelector('.shopping-cart-detail');
+const productDetailContainer = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerButton.addEventListener('click', toggleBurguerMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoMenu);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 
 function toggleDesktopMenu(){
@@ -24,6 +27,18 @@ function toggleCarritoMenu(){
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
 }
+function openProductDetailAside() {
+    productDetailContainer.classList.remove('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.add('inactive');
+}
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.add('inactive');
+}
 
 const productList=[];
     productList.push({name:'Bike',price:120,image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',});
@@ -37,13 +52,19 @@ function renderProducts(arr){
     for(product of arr){
         const productCard=document.createElement('div');
         productCard.classList.add('product-card');
+
         const productImg=document.createElement('img');
         productImg.setAttribute('src',product.image);
+        productImg.addEventListener('click', openProductDetailAside);
+
         const productInfo=document.createElement('div');
         productInfo.classList.add('product-info');
+
         const productInfoDiv=document.createElement('div');
+
         const productPrice=document.createElement('p');
         productPrice.innerText= '$'+ product.price;
+
         const productName=document.createElement('p');
         productName.innerText=product.name;
 
